6519ec17c4a0b52e32ae6d55158218b034fb2280
Yi <andrew.hanyi@gmail.com>
2015-09-29 16:07:41 +1000

    add tag to newproduct

--- experiences/models.py
+++ experiences/models.py
@@ -271,6 +271,7 @@ class NewProduct(AbstractExperience):
     free_translation = models.BooleanField(default=False)
     order_on_holiday = models.BooleanField(default=False, help_text="If supplier take order during weekend and holiday "
                                                                     "particularly instant order during holiday")
+    tags = models.ManyToManyField(ExperienceTag, related_name='newproduct_tags')
 
     def __str__(self):
         t = self.get_product_title(settings.LANGUAGES[0][0])
