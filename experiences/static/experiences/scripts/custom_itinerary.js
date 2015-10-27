135dc1134fffab44781ede70815196108e76e1e0
kemblesong <kemblesong@gmail.com>
2015-10-27 11:50:23 +1100

    #290 new item boxes

--- experiences/static/experiences/scripts/custom_itinerary.js
+++ experiences/static/experiences/scripts/custom_itinerary.js
@@ -45,4 +45,59 @@ $(function() {
     icon.toggleClass("ui-icon-minusthick ui-icon-plusthick");
     icon.closest(".portlet").find(".portlet-content").toggle();
   });
+
+  //Add item buttons (left panel)
+  $("#add-flight-btn").click(function() {
+    $("#flight_column").hide();
+    $("#add-flight").show();
+  });
+  $("#add-flight-cancel").click(function() {
+    $("#flight_column").show();
+    $("#add-flight").hide();
+  });
+
+  $("#add-transfer-btn").click(function() {
+    $("#transfer_column").hide();
+    $("#add-transfer").show();
+  });
+  $("#add-transfer-cancel").click(function() {
+    $("#transfer_column").show();
+    $("#add-transfer").hide();
+  });
+
+  $("#add-accommodation-btn").click(function() {
+    $("#accommodation_column").hide();
+    $("#add-accommodation").show();
+  });
+  $("#add-accommodation-cancel").click(function() {
+    $("#accommodation_column").show();
+    $("#add-accommodation").hide();
+  });
+
+  $("#add-restaurant-btn").click(function() {
+    $("#restaurant_column").hide();
+    $("#add-restaurant").show();
+  });
+  $("#add-restaurant-cancel").click(function() {
+    $("#restaurant_column").show();
+    $("#add-restaurant").hide();
+  });
+
+  $("#add-suggestion-btn").click(function() {
+    $("#suggestion_column").hide();
+    $("#add-suggestion").show();
+  });
+  $("#add-suggestion-cancel").click(function() {
+    $("#suggestion_column").show();
+    $("#add-suggestion").hide();
+  });
+
+  $("#add-pricing-btn").click(function() {
+    $("#pricing_column").hide();
+    $("#add-pricing").show();
+  });
+  $("#add-pricing-cancel").click(function() {
+    $("#pricing_column").show();
+    $("#add-pricing").hide();
+  });
 });
