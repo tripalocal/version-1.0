31b612f261c76f0eca536caf04320ac56b000534
kemblesong <kemblesong@gmail.com>
2016-02-10 13:02:48 +1100

    try removing html tags in markdown_filter

--- experiences/templatetags/markdown_filter.py
+++ experiences/templatetags/markdown_filter.py
@@ -6,4 +6,4 @@ register = template.Library()
 @register.filter
 def markdown_filter(text):
     # safe_mode governs how the function handles raw HTML
-    return markdown.markdown(text, safe_mode='False') 
+    return markdown.markdown(text, safe_mode='remove') 
