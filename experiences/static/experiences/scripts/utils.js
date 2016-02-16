c093a7595024aeafbf3da0265c1249621c15b766
Yi <andrew.hanyi@gmail.com>
2016-02-16 14:58:12 +1100

    add validation for quick login/signup

---
+++ experiences/static/experiences/scripts/utils.js
@@ -0,0 +1,15 @@
+﻿
+function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp( /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i);
+    return pattern.test(emailAddress);
+};
+
+function isValidName(name) {
+    var pattern = new RegExp(/^['a-z\ ._\u4e00-\u9eff]{1,20}$/i);
+    return pattern.test(name);
+};
+
+function isValidPhoneNumber(number) {
+    var pattern = new RegExp(/^[\+0-9\ ]+$/);
+    return pattern.test(number);
+}
