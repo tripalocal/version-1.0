bd57a0db28474074583601c0fe509cc53dd7be21
Yi <andrew.hanyi@gmail.com>
2016-03-10 18:58:25 +1100

    update unit test experiences.views

--- experiences/tests.py
+++ experiences/tests.py
@@ -9,6 +9,7 @@ import django, django.conf, Tripalocal_V1
 from django.test import TestCase
 from experiences.utils import *
 from experiences.models import *
+from experiences.views import *
 from builtins import ValueError
 
 # TODO: Configure your database in settings.py and sync before running tests.
@@ -80,3 +81,23 @@ class UtilsTest(TestCase):
         self.assertTrue(len(email) == 10 and email.isalnum() and email.islower())
         email = email_account_generator(7, "a")
         self.assertTrue(email=="aaaaaaa")
+
+class ViewsTest(TestCase):
+    def test_set_initial_currency(self):
+        request = self.client.request(method="GET").wsgi_request
+        set_initial_currency(self.client.request(method="GET").wsgi_request)
+        self.assertEqual("AUD", request.session["custom_currency"])
+
+        request.session["custom_currency"] = "CNY"
+        set_initial_currency(request)
+        self.assertEqual("CNY", request.session["custom_currency"])
+
+    def test_convert_experience_price(self):
+        request = self.client.request(method="GET").wsgi_request
+        experience = Experience.objects.get(id=59)
+        self.assertIsNone(convert_experience_price(request, experience))
+
+        request.session["custom_currency"] = "CNY"
+        convert_experience_price(request, experience)
+        self.assertEqual(0, experience.dynamic_price.find("960.0,614.69,"))
+        self.assertEqual("CNY", experience.currency)
